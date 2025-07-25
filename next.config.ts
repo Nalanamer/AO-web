cd597827a287e4fcbe143f17c6679159e1cfab39
Nalanamer <nmerannage@gmail.com>
2025-07-26 09:22:23 +1000

    Mimal teste versino of config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  async exportPathMap() {
-    return {
-      '/': { page: '/' }
-    };
-  }
+  reactStrictMode: true
 };
 
 export default nextConfig;
