20babac01f089fd763d797d9c2296048f5b81245
Nalanamer <nmerannage@gmail.com>
2025-07-26 13:45:07 +1000

    Remove /app/api/hello

--- pages/api/hello/route.ts
+++
@@ -1,7 +0,0 @@
-// app/api/hello/route.ts
-export async function GET() {
-  return new Response(JSON.stringify({ name: 'John Doe' }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  })
-}
